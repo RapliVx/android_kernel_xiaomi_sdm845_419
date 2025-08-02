961a235ae1fba973b506c49e217f49ac741849f4
Rafael J. Wysocki <rafael.j.wysocki@intel.com>
2025-08-02 16:53:31 +0500

    Backport: cpufreq: Add special-purpose fast-switching callback for drivers
    First off, some cpufreq drivers (eg. intel_pstate) can pass hints
    beyond the current target frequency to the hardware and there are no
    provisions for doing that in the cpufreq framework.  In particular,
    today the driver has to assume that it should not allow the frequency
    to fall below the one requested by the governor (or the required
    capacity may not be provided) which may not be the case and which may
    lead to excessive energy usage in some scenarios.
    
    Second, the hints passed by these drivers to the hardware need not be
    in terms of the frequency, so representing the utilization numbers
    coming from the scheduler as frequency before passing them to those
    drivers is not really useful.
    
    Address the two points above by adding a special-purpose replacement
    for the ->fast_switch callback, called ->adjust_perf, allowing the
    governor to pass abstract performance level (rather than frequency)
    values for the minimum (required) and target (desired) performance
    along with the CPU capacity to compare them to.
    
    Also update the schedutil governor to use the new callback instead
    of ->fast_switch if present and if the utilization mertics are
    frequency-invariant (that is requisite for the direct mapping
    between the utilization and the CPU performance levels to be a
    reasonable approximation).
    
    Signed-off-by: Rafael J. Wysocki <rafael.j.wysocki@intel.com>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>

--- include/linux/energy_model.h
+++ include/linux/energy_model.h
@@ -134,7 +134,7 @@ static inline unsigned long em_cpu_energy(struct em_perf_domain *pd,
 	cpu = cpumask_first(to_cpumask(pd->cpus));
 	scale_cpu = arch_scale_cpu_capacity(cpu);
 	cs = &pd->table[pd->nr_cap_states - 1];
-	
+
 	max_util = map_util_perf(max_util);
 	max_util = min(max_util, allowed_cpu_cap);
 	freq = map_util_freq(max_util, cs->frequency, scale_cpu);
